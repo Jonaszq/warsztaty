cc12a67b3d88c8dd381898f4ce03bbfea29aff31
Jonaszq Nowak <kenojyt@gmail.com>
2024-09-24 17:51:16 +0000

    Feat: Frontend

---
+++ src/my_project_frontend/src/components/Contact.vue
@@ -0,0 +1,62 @@
+<template>
+    <header class="header">
+      <div class="contact-list">
+        <a href="https://github.com/Jonaszq" target="_blank" class="contact-item">
+          GitHub
+        </a>
+        <a href="mailto:kenojyt@gmail.com" class="contact-item">
+          Email
+        </a>
+      </div>
+    </header>
+  </template>
+  
+  <style scoped>
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 16px;
+    padding: 20px;
+    background: linear-gradient(to right, #0B1C3C, #1E488F);
+    color: #ffffff;
+    border-radius: 8px;
+    max-width: 1200px;
+    width: 100%;
+    max-height: 400px;
+    height: 100%;
+  }
+  h1 {
+    font-size: 4rem;
+    margin-bottom: 10px;
+    text-shadow: 0 0 3px #000000;
+  }
+  h2 {
+    font-size: 2rem;
+    color: #76c7c0;
+    text-shadow: 0 0 3px #000000;
+  }
+  .contact-list {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    margin-top: 20px;
+  }
+  .contact-item {
+    display: flex;
+    align-items: center;
+    color: #ffffff;
+    text-decoration: none;
+    font-size: 1.2rem;
+    text-shadow: 0 0 3px #000000;
+  }
+  .contact-item i {
+    margin-right: 8px;
+    font-size: 1.5rem;
+    text-shadow: 0 0 3px #000000;
+  }
+  .contact-item:hover {
+    color: #A3CFFF;
+  }
+  </style>
---
+++ src/my_project_frontend/src/components/aboutYou.vue
@@ -0,0 +1,49 @@
+    <template>
+  <section class="about">
+    <h2>About You</h2>
+    <div class="about-content">
+      <p>{{ aboutText }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs',
+  data() {
+    return {
+      aboutText: 'Short description about you :)'
+    };
+  }
+}
+</script>
+
+<style scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 35px 20px;
+  background: #1c1c1e;
+  color: #ffffff;
+  max-width: 1200px;
+  width: 100%;
+  border-radius: 8px;
+  box-shadow: 0 0 32px 6px rgba(30, 72, 143, 1);
+}
+.about h2 {
+  margin: 0 0 10px;
+  font-size: 2rem;
+  color: #1e488f;
+}
+.about-content {
+  margin: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.1);
+  border: 1px solid #A3CFFF;
+  border-radius: 8px;
+  font-size: 1.2rem;
+  line-height: 1.5;
+}
+</style>
+
